212851dca95294dee419e489a291f2e4ccd00e28
lyx0206331 <lengyuxuan1534>
2018-12-06 01:18:35 +0800

    更新HorizontalScrollView指示器

--- indicatorlib/src/main/java/com/adrian/indicatorlib/HorizontalScrollViewIndicator.kt
+++ indicatorlib/src/main/java/com/adrian/indicatorlib/HorizontalScrollViewIndicator.kt
@@ -10,12 +10,13 @@ import android.util.Log
 import android.view.View
 import android.widget.HorizontalScrollView
 import android.widget.LinearLayout
+import java.lang.Exception
 import kotlin.math.log
 
 /**
  * date:2018/12/5 16:48
  * author：RanQing
- * description：
+ * description：水平滚动指示器
  */
 class HorScrollViewIndicator @JvmOverloads constructor(
     context: Context?,
@@ -24,7 +25,7 @@ class HorScrollViewIndicator @JvmOverloads constructor(
 ) : View(context, attrs, defStyleAttr) {
     companion object {
         const val COLOR_BACKGROUND = Color.GRAY
-        const val COLOR_PROGRESS = Color.CYAN
+        const val COLOR_BAR = Color.CYAN
     }
 
     private val bgPaint by lazy {
@@ -34,11 +35,10 @@ class HorScrollViewIndicator @JvmOverloads constructor(
         Paint(Paint.ANTI_ALIAS_FLAG)
     }
     private var mScrollX = 0
-    private var mScrollViewW = 0
+    private var allowScrollWidth = 0
     var mHorScrollView: HorScrollView? = null
         set(value) {
             field = value
-            mScrollViewW = value?.width.orZero()
             value?.scrollViewListener = object : ScrollViewListener {
                 override fun onScrollChanged(scrollView: HorScrollView, x: Int, y: Int, oldX: Int, oldY: Int) {
 //                    logE("scroll x:$x, y:$y, oldx:$oldX, oldy:$oldY")
@@ -51,18 +51,30 @@ class HorScrollViewIndicator @JvmOverloads constructor(
         set(value) {
             field = value
             bgPaint.color = value
+            invalidate()
         }
-    var mBarColor: Int = COLOR_PROGRESS
+    var mBarColor: Int = COLOR_BAR
         set(value) {
             field = value
             barPaint.color = value
+            invalidate()
         }
     var mRadius = 5f
-    var mBarWidth = 0
+        set(value) {
+            field = value
+            invalidate()
+        }
+    private var mBarWidth = 0
 
     init {
-        bgPaint.color = mBgColor
-        barPaint.color = mBarColor
+
+        val a = context?.obtainStyledAttributes(attrs, R.styleable.HorScrollViewIndicator, defStyleAttr, 0)
+        a?.apply {
+            mBgColor = getColor(R.styleable.HorScrollViewIndicator_hsviBgColor, COLOR_BACKGROUND).orZero()
+            mBarColor = getColor(R.styleable.HorScrollViewIndicator_hsviBarColor, COLOR_BAR).orZero()
+            mRadius = getDimension(R.styleable.HorScrollViewIndicator_hsviRadius, 5f)
+        }
+        a?.recycle()
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -81,14 +93,21 @@ class HorScrollViewIndicator @JvmOverloads constructor(
 
     override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
         super.onLayout(changed, left, top, right, bottom)
-        mBarWidth = if (mBarWidth <= 0) width / 2 else mBarWidth
+        if (mHorScrollView?.childCount == 0) {
+            throw Exception("ScrollView must has a child")
+        }
+        val mScrollViewW = mHorScrollView?.width.orZero()
+        val mScrollChildViewW = mHorScrollView?.getChildAt(0)?.width.orZero()
+        allowScrollWidth = mScrollChildViewW - mScrollViewW
+        allowScrollWidth = (if (allowScrollWidth == 0) 1 else allowScrollWidth)
+        mBarWidth = (1f * width * mScrollViewW / mScrollChildViewW).toInt()
+//        mBarWidth = if (mBarWidth <= 0) width / 2 else mBarWidth
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
         val bgRectF = RectF(0f, 0f, width.toFloat(), height.toFloat())
-        val offsetX = 1f * (width - mBarWidth) * mScrollX / (mScrollViewW - width)
-        logE("offsetX:$offsetX, (width-barW):${width - mBarWidth}, width:$width")
+        val offsetX = 1f * (width - mBarWidth) * mScrollX / allowScrollWidth
         val barRectF = RectF(offsetX, 0f, offsetX + mBarWidth, height.toFloat())
         canvas?.drawRoundRect(bgRectF, mRadius, mRadius, bgPaint)
         canvas?.drawRoundRect(barRectF, mRadius, mRadius, barPaint)
