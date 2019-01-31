fb3d5cc00042db1b28035d37226aed2405d7cacd
lyx0206331 <rq880113@163.com>
2019-01-31 11:08:21 +0800

    更新HorScrollView指示器

--- indicatorlib/src/main/java/com/adrian/indicatorlib/ViewPagerIndicator.kt
+++ indicatorlib/src/main/java/com/adrian/indicatorlib/ViewPagerIndicator.kt
@@ -7,11 +7,19 @@ import android.graphics.drawable.GradientDrawable
 import android.util.AttributeSet
 import android.util.Log
 import android.util.TypedValue
+import android.view.Gravity
 import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.RelativeLayout
 import androidx.annotation.ColorInt
+import androidx.core.content.ContextCompat
+import androidx.dynamicanimation.animation.FloatPropertyCompat
+import androidx.dynamicanimation.animation.SpringAnimation
+import androidx.dynamicanimation.animation.SpringForce
 import androidx.viewpager.widget.ViewPager
 import kotlin.math.abs
 
@@ -243,6 +251,223 @@ class DotsIndicator @JvmOverloads constructor(context: Context, attrs: Attribute
         }
     }
 
+}
+
+class WormDotsIndicator @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : FrameLayout(context, attrs, defStyleAttr) {
+
+    private var strokeDots = arrayListOf<ImageView>()
+    private var dotIndicatorView: ImageView? = null
+    //    private var dotIndicatorLayout: View? = null
+    private var viewPager: ViewPager? = null
+
+    var dotsSize = dp2px(context, 16).toInt()
+    var dotsCornerRadius = dotsSize / 2f
+    var dotsSpacing = dp2px(context, 4).toInt()
+    var dotsStrokeWidth = dp2px(context, 2).toInt()
+    var dotIndicatorColor = getThemePrimaryColor(context)
+    var dotsStrokeColor = dotIndicatorColor
+
+    var horizontalMargin: Int = dp2px(context, 24).toInt()
+    var dotIndicatorXSpring: SpringAnimation? = null
+    var dotIndicatorWidthSpring: SpringAnimation? = null
+    var strokeDotsLinearLayout: LinearLayout? = null
+
+    var dotsClickable = true
+    var pageChangedListener: ViewPager.OnPageChangeListener? = null
+
+    init {
+        strokeDotsLinearLayout = LinearLayout(context)
+        val lp = LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        lp.setMargins(horizontalMargin, 0, horizontalMargin, 0)
+        strokeDotsLinearLayout?.layoutParams = lp
+        strokeDotsLinearLayout?.orientation = LinearLayout.HORIZONTAL
+        addView(strokeDotsLinearLayout)
+
+        attrs?.apply {
+            val a = context.obtainStyledAttributes(this, R.styleable.WormDotsIndicator, defStyleAttr, 0)
+            dotIndicatorColor = a.getColor(R.styleable.WormDotsIndicator_dotsColor, dotIndicatorColor)
+            dotsStrokeColor = a.getColor(R.styleable.WormDotsIndicator_dotsStrokeColor, dotsStrokeColor)
+            dotsSize = a.getDimensionPixelSize(R.styleable.WormDotsIndicator_dotsSize, dotsSize)
+            dotsSpacing = a.getDimensionPixelSize(R.styleable.WormDotsIndicator_dotsSpacing, dotsSpacing)
+            dotsCornerRadius = a.getDimension(R.styleable.WormDotsIndicator_dotsCornerRadius, dotsCornerRadius)
+            dotsStrokeWidth = a.getDimensionPixelSize(R.styleable.WormDotsIndicator_dotsStrokeWidth, dotsStrokeWidth)
+
+            a.recycle()
+        }
+
+        if (isInEditMode) {
+            addStrokeDots(5)
+            addView(buildDot(false))
+        }
+    }
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        refreshDots()
+    }
+
+    private fun refreshDots() {
+        if (dotIndicatorView == null) {
+            setupDotIndicator()
+        }
+        if (viewPager == null || viewPager?.adapter == null) {
+            Log.e(WormDotsIndicator::class.java.simpleName, "You have to set an adapter to the view pager before !")
+        } else {
+            viewPager?.adapter?.apply {
+                if (strokeDots.size < count) {
+                    addStrokeDots(count - strokeDots.size)
+                } else if (strokeDots.size > count) {
+                    removeDots(strokeDots.size - count)
+                }
+                setupDotsAnimators()
+            }
+        }
+    }
+
+    private fun setupDotIndicator() {
+        dotIndicatorView = buildDot(false)
+        addView(dotIndicatorView)
+        dotIndicatorXSpring = SpringAnimation(dotIndicatorView, SpringAnimation.TRANSLATION_X)
+        val springForceX = SpringForce(0f)
+        springForceX.dampingRatio = 1f
+        springForceX.stiffness = 300f
+        dotIndicatorXSpring?.spring = springForceX
+
+        val floatPropertyCompat = object : FloatPropertyCompat<Any>("DotsWidth") {
+            override fun setValue(`object`: Any?, value: Float) {
+                val params = dotIndicatorView?.layoutParams
+                params?.width = value.toInt()
+                dotIndicatorView?.requestLayout()
+            }
+
+            override fun getValue(`object`: Any?): Float {
+                return dotIndicatorView?.layoutParams?.width.orZero().toFloat()
+            }
+        }
+        dotIndicatorWidthSpring = SpringAnimation(dotIndicatorView, floatPropertyCompat)
+        val springForceWidth = SpringForce(0f)
+        springForceWidth.dampingRatio = 1f
+        springForceWidth.stiffness = 300f
+        dotIndicatorXSpring?.spring = springForceWidth
+    }
+
+    private fun addStrokeDots(count: Int) {
+        for (i in 0 until count) {
+            val dot = buildDot(true)
+            dot.setOnClickListener {
+                viewPager?.adapter?.apply {
+                    if (dotsClickable && i < this.count) {
+                        viewPager?.setCurrentItem(i, true)
+                    }
+                }
+            }
+            strokeDots.add(dot)
+            strokeDotsLinearLayout?.addView(dot)
+        }
+    }
+
+    private fun buildDot(stroke: Boolean): ImageView {
+        val dotImageView = ImageView(context)
+        dotImageView.background = ContextCompat.getDrawable(
+            context,
+            if (stroke) R.drawable.worm_dot_stroke_background else R.drawable.worm_dot_background
+        )
+        val lp = FrameLayout.LayoutParams(dotsSize, dotsSize)
+        lp.gravity = Gravity.CENTER_VERTICAL
+        lp.setMargins(dotsSpacing, 0, dotsSpacing, 0)
+        dotImageView.layoutParams = lp
+
+        setupDotBackground(stroke, dotImageView)
+        return dotImageView
+    }
+
+    private fun setupDotBackground(stroke: Boolean, dotImageView: ImageView) {
+        val dotBackground = dotImageView.background as GradientDrawable
+        if (stroke) {
+            dotBackground.setStroke(dotsStrokeWidth, dotsStrokeColor)
+        } else {
+            dotBackground.setColor(dotIndicatorColor)
+        }
+        dotBackground.cornerRadius = dotsCornerRadius
+    }
+
+    private fun removeDots(count: Int) {
+        for (i in 0 until count) {
+            strokeDotsLinearLayout?.removeViewAt(strokeDotsLinearLayout?.childCount.orZero() - 1)
+            strokeDots.removeAt(strokeDots.size - 1)
+        }
+    }
+
+    private fun setupDotsAnimators() {
+        viewPager?.let {
+            it.adapter?.apply {
+                pageChangedListener?.apply {
+                    if (count > 0) {
+                        it.removeOnPageChangeListener(this)
+                    }
+                    setupOnPageChangedListener()
+                    it.addOnPageChangeListener(this)
+                }
+            }
+        }
+    }
+
+    private fun setupOnPageChangedListener() {
+        pageChangedListener = object : ViewPager.OnPageChangeListener {
+            override fun onPageScrollStateChanged(state: Int) {
+            }
+
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+                val stepX = dotsSize + dotsSpacing * 2
+                var xFinalPosition: Float = 0f
+                var widthFinalPosition: Float
+                if (positionOffset >= 0 && positionOffset < .1f) {
+                    xFinalPosition = (horizontalMargin + position * stepX).toFloat()
+                    widthFinalPosition = dotsSize.toFloat()
+                } else if (positionOffset in .1f...9f) {
+                    xFinalPosition = (horizontalMargin + position * stepX).toFloat()
+                    widthFinalPosition = dotsSize.toFloat() + stepX
+                } else {
+                    xFinalPosition = (horizontalMargin + (position + 1) * stepX).toFloat()
+                    widthFinalPosition = dotsSize.toFloat()
+                }
+                if (dotIndicatorXSpring?.spring?.finalPosition != xFinalPosition) {
+                    dotIndicatorXSpring?.spring?.finalPosition = xFinalPosition
+                }
+
+                if (dotIndicatorWidthSpring?.spring?.finalPosition != widthFinalPosition) {
+                    dotIndicatorWidthSpring?.spring?.finalPosition = widthFinalPosition
+                }
+
+                if (!dotIndicatorXSpring?.isRunning.orFalse()) {
+                    dotIndicatorXSpring?.start()
+                }
+
+                if (!dotIndicatorWidthSpring?.isRunning.orFalse()) {
+                    dotIndicatorWidthSpring?.start()
+                }
+            }
+
+            override fun onPageSelected(position: Int) {
+            }
+
+        }
+    }
+
+    private fun setupViewPager() {
+        viewPager?.adapter?.apply {
+            this.registerDataSetObserver(object : DataSetObserver() {
+                override fun onChanged() {
+                    super.onChanged()
+                    refreshDots()
+                }
+            })
+        }
+    }
 }
 
 fun dp2px(context: Context, dp: Int): Float {
